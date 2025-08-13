77b31de821a603f9a02f9979094e316a257b36ba
aliyah0231 <105841106923@student.unismuh.ac.id>
2025-08-13 20:45:28 +0800

    Perbaiki login

--- hash-pw.js
+++ hash-pw.js
@@ -1,7 +1,8 @@
-const bcrypt = require("bcrypt");
+import bcrypt from "bcrypt";
 
-const plainPassword = "Admin123"; 
+const plainPassword = "Admin123"; // password yang kamu masukkan di form
+const hashFromDB = "$2b$10$GqIKRTNJkSLz3FfmUJwjG.SV/UpibLBrAkcukbkET/L6BvZ432udO"; // paste hash dari kolom password_hash
 
-bcrypt.hash(plainPassword, 10).then(hash => {
-  console.log("Password hash:", hash);
+bcrypt.compare(plainPassword, hashFromDB).then(match => {
+  console.log("Password cocok?", match);
 });
